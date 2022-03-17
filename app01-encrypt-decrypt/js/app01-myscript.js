ef8919b4e512f1fef0ab49b957b93a35e35533fe
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-03-17 22:02:31 +0200

    Update app01-myscript.js

--- app01-encrypt-decrypt/js/app01-myscript.js
+++ app01-encrypt-decrypt/js/app01-myscript.js
@@ -1,7 +1,6 @@
 async function demodemo() {
-    
-    event.preventDefault() ; 
-    
+    event.preventDefault() ; // Add this line at the top to work with bootstrap forms
+    //
     let inputTextBox1message = document.getElementById("inputTextBox1").value;
     let passphrase = document.getElementById("secretPassphrase").value;
     //const outputEncryptedTextArea = document.getElementById("outputEncryptedTextArea").value ;
@@ -25,8 +24,37 @@ async function demodemo() {
     //////////////////////////////////////////////////
 
     // put keys in backtick (``) to avoid errors caused by spaces or tabs
-    const publicKeyArmored = publicKey; // encrypted public key
-    const privateKeyArmored = privateKey; // encrypted private key
+    const publicKeyArmored = `-----BEGIN PGP PUBLIC KEY BLOCK-----
+    
+xjMEYjORhRYJKwYBBAHaRw8BAQdAwqEqhe8zDK2vscWV5a2suE1ctfRJsNHN
+MOOK6hv9rp3NG0pvbiBTbWl0aCA8am9uQGV4YW1wbGUuY29tPsKMBBAWCgAd
+BQJiM5GFBAsJBwgDFQgKBBYAAgECGQECGwMCHgEAIQkQPP9y68ymayMWIQQS
+heQmPLYapWaMDbw8/3LrzKZrI43UAQC6C7kYmOZYfxO1QO4/a95mprlNJ88l
+8HN3SNv6CuVz/wEAw00aUpDr648Qp6sA4BDlsn1A7KjhTDXhabK1TFweUgPO
+OARiM5GFEgorBgEEAZdVAQUBAQdAwG3fciz0IfPLTde5fO2oP44kijfUTm/L
+dUy0ir35Z00DAQgHwngEGBYIAAkFAmIzkYUCGwwAIQkQPP9y68ymayMWIQQS
+heQmPLYapWaMDbw8/3LrzKZrI4GtAP9hgaGEM15sW6J+jQ68dGJ4S5ni3Eba
+sXarAVF2E4nx5gD/UNaitb7k9YvUm2PbV6YnecSzgiuorerot5Qt3qa1UAM=
+=u/Hi
+-----END PGP PUBLIC KEY BLOCK-----`; // encrypted public key
+
+    const privateKeyArmored = `-----BEGIN PGP PRIVATE KEY BLOCK-----
+
+xYYEYjORhRYJKwYBBAHaRw8BAQdAwqEqhe8zDK2vscWV5a2suE1ctfRJsNHN
+MOOK6hv9rp3+CQMIbAcEgKy5YRngEyQ5h11pJQTh/V8KvhYPf7ktzk2m0dj/
+R47ynCpu4vtcHkqzDhBhtVbN5tVEUWpTYBtyDLDR8J4fksGfrGSGABU9vxwe
+Ws0bSm9uIFNtaXRoIDxqb25AZXhhbXBsZS5jb20+wowEEBYKAB0FAmIzkYUE
+CwkHCAMVCAoEFgACAQIZAQIbAwIeAQAhCRA8/3LrzKZrIxYhBBKF5CY8thql
+ZowNvDz/cuvMpmsjjdQBALoLuRiY5lh/E7VA7j9r3mamuU0nzyXwc3dI2/oK
+5XP/AQDDTRpSkOvrjxCnqwDgEOWyfUDsqOFMNeFpsrVMXB5SA8eLBGIzkYUS
+CisGAQQBl1UBBQEBB0DAbd9yLPQh88tN17l87ag/jiSKN9ROb8t1TLSKvfln
+TQMBCAf+CQMIJ70d/dOU9nHgp6Q5delpXCHI3iBUpQYLkFRIS/bQs7iEHBa2
+IEzj3CBBlbySjmNUkhfE3YuMpHiS42MbtIDnWOm7mGniqTQNPjLMRu7TC8J4
+BBgWCAAJBQJiM5GFAhsMACEJEDz/cuvMpmsjFiEEEoXkJjy2GqVmjA28PP9y
+68ymayOBrQD/YYGhhDNebFuifo0OvHRieEuZ4txG2rF2qwFRdhOJ8eYA/1DW
+orW+5PWL1Jtj21emJ3nEs4IrqK3q6LeULd6mtVAD
+=D7Sn
+-----END PGP PRIVATE KEY BLOCK-----`; // encrypted private key
 
     // READ PUBLIC KEY
     const publicKeyNew = await openpgp.readKey({ armoredKey: publicKeyArmored });
@@ -36,7 +64,7 @@ async function demodemo() {
         privateKey: await openpgp.readPrivateKey({ armoredKey: privateKeyArmored }),
         passphrase
     });
-    console.log(privateKeyNew);
+    //console.log(privateKeyNew);
     console.log(passphrase);
 
     const encrypted = await openpgp.encrypt({
