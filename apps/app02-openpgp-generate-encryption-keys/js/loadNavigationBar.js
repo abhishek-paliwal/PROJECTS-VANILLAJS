eb9b76b769ffa6b69d10a809d95009e2797cae01
Abhishek Paliwal <abhishek.paliwal@concepro.com>
2022-03-20 16:23:36 +0200

    redone

--- apps/app02-openpgp-generate-encryption-keys/js/loadNavigationBar.js
+++ apps/app02-openpgp-generate-encryption-keys/js/loadNavigationBar.js
@@ -2,14 +2,17 @@ function loadNavigationBar() {
     //
     const displayContentForNavBar = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="https://www.abhishekpali.us">
+      <img src="https://downloads.concepro.com/dropbox-public-files/logos/1-logos-pali/Favicon-Pali-Website.png" alt="" width="24" height="24" class="d-inline-block align-text-top">
+      Apps-List
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="https://www.abhishekpali.us">Homepage</a>
+          <a class="nav-link" href="https://www.abhishekpali.us">AbhishekPali.us</a>
         </li>
       </ul>
     </div>
